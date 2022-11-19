d529bc161c82c9fca4db127d54b09842dec8632e
Menbalec <Michel.menbalec@gmail.com>
2022-11-19 10:15:52 +0100

    et encore

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ import math
 
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
-line = [["A"," "," ","B"," "," ","C"," "," ","0  "],
+ligne = [
+        ["A"," "," ","B"," "," ","C"," "," ","0  "],
         ["|"," "," ","|"," "," ","|"," "," ","1  "],
         ["|","-","-","|"," "," ","|"," "," ","2  "],
         ["|"," "," ","|","-","-","|"," "," ","3  "],
         ["|"," "," ","|","-","-","|"," "," ","4  "],
         ["|"," "," ","|"," "," ","|"," "," ","5  "],
         ["1"," "," ","2"," "," ","3"," "," ","6  "],
+        ]
 
 w, h = [int(i) for i in input().split()]
 for i in range(h):
-    line = input()
+    line = ligne[i]
     print("Debug messages...",line[4]," ", i, file=sys.stderr, flush=True)
     print("Debug messages...",line," ", i, file=sys.stderr, flush=True)
     
