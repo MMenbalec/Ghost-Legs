2f69ceb1fc1613d98c5e09362a820971cd2fab34
Menbalec <Michel.menbalec@gmail.com>
2022-11-19 10:13:00 +0100

    mise a jour variable

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import math
 
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
+line = [["A"," "," ","B"," "," ","C"," "," ","0  "],
+        ["|"," "," ","|"," "," ","|"," "," ","1  "],
+        ["|","-","-","|"," "," ","|"," "," ","2  "],
+        ["|"," "," ","|","-","-","|"," "," ","3  "],
+        ["|"," "," ","|","-","-","|"," "," ","4  "],
+        ["|"," "," ","|"," "," ","|"," "," ","5  "],
+        ["1"," "," ","2"," "," ","3"," "," ","6  "],
 
 w, h = [int(i) for i in input().split()]
 for i in range(h):
